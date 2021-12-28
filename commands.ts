ac5f93cc8bc41d8279c3d8ff08d6bc5082967d14
lilnasy <emmagoldmanvevo@pm.me>
2021-12-28 11:32:57 +0000

    formatting

--- commands.ts
+++ commands.ts
@@ -1,79 +1,115 @@
-export	{ auth
-	, expire
-	, get
-	, hset
-	, hget
-	, set
-	, setex
-	, zadd
-	, zcount
-	, zremrangebyscore }
-
-function auth(password: string) {
-	return ["AUTH", password]
+export function auth(
+	password: string
+) {
+	return [
+		'AUTH',
+		password
+	]
 }
 
-function expire(
+export function expire(
 	key: string,
-	seconds: number ) {
-	
-	return ["EXPIRE", key, String(seconds)]
+	seconds: number
+) {
+	return [
+		'EXPIRE',
+		key,
+		String(seconds)
+	]
 }
 
-function get(key: string) {
-	return ["GET", key]
+export function get(
+	key: string
+) {
+	return [
+		'GET',
+		key
+	]
 }
 
-function hset(
+export function hset(
 	key: string,
 	field: string,
-	value: string ) {
-	
-	return ["HSET", key, field, value]
+	value: string
+) {
+	return [
+		'HSET',
+		key,
+		field,
+		value
+	]
 }
 
-function hget(
+export function hget(
 	key: string,
-	field: string ) {
-	
-	return ["HGET", key, field]
+	field: string
+) {
+	return [
+		'HGET',
+		key,
+		field
+	]
 }
 
-function set(
+export function set(
 	key: string,
-	value: string ) {
-	
-	return ["SET", key, value]
+	value: string
+) {
+	return [
+		'SET',
+		key,
+		value
+	]
 }
 
-function setex(
+export function setex(
 	key: string,
 	ttl: number,
-	value: string ) {
-	
-	return ["SETEX", key, String(ttl), value]
+	value: string
+) {
+	return [
+		'SETEX',
+		key,
+		String(ttl),
+		value
+	]
 }
 
-function zadd(
+export function zadd(
 	nameOfSet: string,
 	score: number,
-	nameOfMember: string ) {
-	
-	return ["ZADD", nameOfSet, String(score), nameOfMember]
+	nameOfMember: string
+) {
+	return [
+		'ZADD',
+		nameOfSet,
+		String(score),
+		nameOfMember
+	]
 }
 
-function zcount(
+export function zcount(
 	nameOfSet: string,
 	startOfRange: number,
-	endOfRange: number ) {
-	
-	return ["ZCOUNT", nameOfSet, String(startOfRange), String(endOfRange)]
+	endOfRange: number
+) {
+	return [
+		'ZCOUNT',
+		nameOfSet,
+		String(startOfRange),
+		String(endOfRange)
+	]
 }
 
-function zremrangebyscore(
+export function zremrangebyscore(
 	nameOfSet: string,
 	startOfRange: number,
-	endOfRange: number ) {
-	
-	return ["ZREMRANGEBYSCORE", nameOfSet, String(startOfRange), String(endOfRange)]
+	endOfRange: number
+) {
+	return [
+		'ZREMRANGEBYSCORE',
+		nameOfSet,
+		String(startOfRange),
+		String(endOfRange),
+	]
 }
--- protocol.ts
+++ protocol.ts
@@ -1,24 +1,21 @@
-export  { type RESP
-        , type RedisRequest
-        , type RedisResponse
-        , encodeRESP
-        , decodeRESP }
-
 // CRLF delimited text
-type RESP = string
+export type RESP = string
 
 // array of bulkstrings
-type RedisRequest = Array<string>
+export type RedisRequest = Array<string>
 
-type RedisResponse = string | number | null | Array<RedisResponse>
-
+export type RedisResponse = string | number | null | Array<RedisResponse>
 
 /** translates commands to redis protocol 2 for sending to the redis connection
  * https://web.archive.org/web/20211127173832/https://redis.io/topics/protocol
  * @param bulkstrings array of strings for a single command
-*/
-function encodeRESP(bulkstrings: RedisRequest): RESP {
-	const main = bulkstrings.flatMap( bulk => ['$' + bulk.length, bulk] ).join('\r\n')
+ */
+export function encodeRESP(bulkstrings: RedisRequest): RESP {
+	
+	const main = bulkstrings
+		.flatMap( bulk => ['$' + bulk.length, bulk])
+		.join('\r\n')
+	
 	return '*' + bulkstrings.length + '\r\n' + main + '\r\n'
 }
 
@@ -26,37 +23,61 @@ function encodeRESP(bulkstrings: RedisRequest): RESP {
  * each message usually contains response to one command
  * @param resp string encoded in resp
  */
-function decodeRESP(resp: RESP): Array<RedisResponse> {
+export function decodeRESP(resp: RESP): Array<RedisResponse> {
 	// recursive implementation of the Redis Protocol RESP2
 	// not tail-recursive because priorities
-	switch ( resp[0] ) {
-
+	switch (resp[0]) {
 		case '-':
 		case '+': {
-			const simpleString	= resp.substring( 1, resp.indexOf('\r\n') )
-			const rest		= resp.substring( resp.indexOf('\r\n') + 2 )
-			return [ simpleString , ...decodeRESP(rest) ] }
+			
+			const simpleString = resp.substring(1, resp.indexOf('\r\n'))
+			
+			const rest = resp.substring(resp.indexOf('\r\n') + 2)
+			
+			return [simpleString, ...decodeRESP(rest)]
+		}
 
 		case ':': {
-			const integer		= Number( resp.substring( 1, resp.indexOf('\r\n') ) )
-			const rest		= resp.substring( resp.indexOf('\r\n') + 2 )
-			return [ integer, ...decodeRESP(rest) ] }
+			
+			const integer = Number(resp.substring(1, resp.indexOf('\r\n')))
+			
+			const rest = resp.substring(resp.indexOf('\r\n') + 2)
+			
+			return [integer, ...decodeRESP(rest)]
+		}
 
 		case '$': {
-			const _bulkStringLength	= resp.substring( 1, resp.indexOf('\r\n') )
-			const rest		= resp.substring( resp.indexOf('\r\n') + 2 )
-			const bulkString	= rest.substring( 0, rest.indexOf('\r\n') )
-			const restrest		= rest.substring( rest.indexOf('\r\n') + 2 )
-			return [ bulkString , ...decodeRESP(restrest) ] }
+			
+			const _bulkStringLength = resp.substring(1, resp.indexOf('\r\n'))
+			
+			const rest = resp.substring(resp.indexOf('\r\n') + 2)
+			
+			const bulkString = rest.substring(0, rest.indexOf('\r\n'))
+			
+			const restrest = rest.substring(rest.indexOf('\r\n') + 2)
+			
+			return [bulkString, ...decodeRESP(restrest)]
+		}
 
 		case '*': {
-			const arrayLength	= Number( resp.substring( 1, resp.indexOf('\r\n') ) )
-			const rest		= resp.substring( resp.indexOf('\r\n') + 2 )
-			const restDecoded	= decodeRESP(rest)
-			if (arrayLength === -1)	return [ null, ...restDecoded ]
-			if (arrayLength === 0)	return [ [], ...restDecoded ]
-			return [ restDecoded.slice( 0, arrayLength ), ...restDecoded.slice( arrayLength ) ] }
+			
+			const arrayLength = Number(resp.substring(1, resp.indexOf('\r\n')))
+			
+			const rest = resp.substring(resp.indexOf('\r\n') + 2)
+			
+			const restDecoded = decodeRESP(rest)
+			
+			if (arrayLength === -1) return [null, ...restDecoded]
+			
+			if (arrayLength === 0) return [[], ...restDecoded]
+			
+			return [
+				restDecoded.slice(0, arrayLength),
+				...restDecoded.slice(arrayLength),
+			]
+		}
 
-		default	: return []
+		default:
+			return []
 	}
 }
